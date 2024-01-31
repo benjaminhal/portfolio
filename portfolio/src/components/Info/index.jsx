179819309df8367919e921fb3724a4b2334b785d
halajda <halajdabenj@gmail.com>
2024-01-31 16:46:45 +0100

    projet final

--- portfolio/src/components/Info/index.jsx
+++ portfolio/src/components/Info/index.jsx
@@ -9,15 +9,15 @@ function Info() {
             <div className='info-perso'>
 					<div className='tel-info'>
 						<img src={Tel} alt="tel"/>
-						<p> 07 82 47 18 66</p>
+						<p> 07 82 47 18 60</p>
 					</div>	
 					<div className='mail-info'>
 						<img src={Mail} alt="mail"/>
-						<p>benjamin.halajda@gmail.com</p>
+						<p>benjamin.halda@gmail.com</p>
 					</div>
                     <div className="adresse">
                         <img src={Adresse} alt="adresse"/>
-                        <p>3 résidence la closerie, 59135, Bellaing</p>
+                        <p>3 résidence , 59135, Bellaing</p>
                     </div>
                     <h1 className="cc">
                         N'hesitez pas à me contacter pour plus d'informations me concernant. Je reste à votre disposition pour une propostion d'offre d'emploie, d'entretient ou tout autre réalisation de projet.
